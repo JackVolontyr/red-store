9e78317e350cf971f46d1e46e82e7528e7104fe7
JackVolontyr <soapforwork@gmail.com>
2020-04-14 16:17:01 +0300

    routing example

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { HomePage, CartPage } from '../Pages';
+import './App.css';
 
 const App = () => {
-  return <div>Hi!</div>
+  return <div className="rs-app">
+    <Switch>
+      <Route path='/' component={HomePage} exact />
+      <Route path='/cart' component={CartPage} />
+    </Switch>
+  </div>
 }
 
 export default App;
