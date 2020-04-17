1fe39d8a2adedf421fb51a173c4c3c1db287eb6d
JackVolontyr <soapforwork@gmail.com>
2020-04-17 14:35:24 +0300

    enhancers, middlewares

--- src/store.js
+++ src/store.js
@@ -1,6 +1,25 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducers";
 
-const store = createStore(reducer);
+// .... = ({getState, dispatch}) => (next)     => (action) => {
+const stringMiddleware = (store) => (dispatch) => (action) => {
+  if (typeof action === 'string') return dispatch({ type: action });
+  return dispatch(action);
+}
+
+const logMiddleware = (store) => (dispatch) => (action) => {
+  console.log(action.type);
+  return dispatch(action);
+}
+
+const store = createStore(
+  // ...., storeEnhancer
+  reducer, applyMiddleware(
+    stringMiddleware,
+    logMiddleware
+  )
+);
+
+store.dispatch('TEST');
 
 export default store;
