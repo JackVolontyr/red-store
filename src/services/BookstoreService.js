abea86f2f87641e2ea78bb9e81bac2c8f1eafde0
JackVolontyr <soapforwork@gmail.com>
2020-04-19 14:15:51 +0300

    update BookstoreService

--- src/services/BookstoreService.js
+++ src/services/BookstoreService.js
@@ -7,6 +7,8 @@ export default class BookstoreService {
     currency: '$',
     quantity: 0,
     cover: 'https://book-ye.com.ua/upload/iblock/277/99c35223_35ba_11e8_80f0_000c29ae1566_ee9e5d79_35ba_11e8_80f0_000c29ae1566.jpg',
+    isInCart: false,
+    rating: 2,
   }, {
     id: 2,
     title: 'Treasure Island',
@@ -14,7 +16,9 @@ export default class BookstoreService {
     price: 50,
     currency: '$',
     quantity: 1,
-    cover: 'https://images-na.ssl-images-amazon.com/images/I/910CGpvNnkL.jpg'
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/910CGpvNnkL.jpg',
+    isInCart: false,
+    rating: 1,
   }, {
     id: 3,
     title: 'Robinson Crusoe',
@@ -22,7 +26,10 @@ export default class BookstoreService {
     price: 37,
     currency: '$',
     quantity: 5,
-    cover: 'https://images-na.ssl-images-amazon.com/images/I/414p0x7KV%2BL.jpg'
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/414p0x7KV%2BL.jpg',
+    isInCart: false,
+    rating: 5,
+
   }, {
     id: 4,
     title: 'The Sea Wolf',
@@ -30,7 +37,10 @@ export default class BookstoreService {
     price: 33,
     currency: '$',
     quantity: 3,
-    cover: 'https://kbimages1-a.akamaihd.net/602eabe2-2158-44df-ba9e-29ae8849493e/1200/1200/False/the-sea-wolf-14.jpg'
+    cover: 'https://kbimages1-a.akamaihd.net/602eabe2-2158-44df-ba9e-29ae8849493e/1200/1200/False/the-sea-wolf-14.jpg',
+    isInCart: false,
+    rating: 2,
+
   }, {
     id: 5,
     title: 'Moby-Dick, or The Whale',
@@ -38,12 +48,15 @@ export default class BookstoreService {
     price: 10,
     currency: '$',
     quantity: 2,
-      cover: 'https://www.booklya.ua/content/upload/product/183k/183841/800x800/560616/moby-dick.jpg'
+    cover: 'https://www.booklya.ua/content/upload/product/183k/183841/800x800/560616/moby-dick.jpg',
+    isInCart: false,
+    rating: 3,
+
   }];
 
   getBooks = () => new Promise((resolve, reject) => setTimeout(() => {
-    if (Math.random() > 0.9) {
-    // if (false) {
+    // if (Math.random() > 0.9) {
+    if (false) {
       reject(new Error('Error.'));
 
     } else {
